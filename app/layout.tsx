7bd6ebd34f659b37c9532bd413ac8c199e8a879b
Isaac Lee <60437099+dltkr12311@users.noreply.github.com>
2024-07-08 16:57:11 +0900

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import Footer from './components/footer';
+import { Navbar } from './components/nav';
+import './global.css';
+import { baseUrl } from './sitemap';
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Isaac Portfolio',
     description: 'This is my portfolio.',
     url: baseUrl,
     siteName: 'My Portfolio',
@@ -34,26 +34,26 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-}
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
-      lang="en"
+      lang='en'
       className={cx(
         'text-black bg-white dark:text-white dark:bg-black',
         GeistSans.variable,
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className='antialiased max-w-xl mx-4 mt-8 lg:mx-auto'>
+        <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
           <Navbar />
           {children}
           <Footer />
@@ -62,5 +62,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
